388945e03d7b502920678bec2dce2a420caec524
Karlson Drendel <kdrendel99@gmail.com>
2021-05-06 14:18:57 -0700

    Added second controller for Vendors

--- OrderTracker/Controllers/HomeController.cs
+++ OrderTracker/Controllers/HomeController.cs
@@ -1,32 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using OrderTracker.Models;
-using System.Collections.Generic;
 
 namespace OrderTracker.Controllers
 {
-  public class HomeController : Controller
-  {
-    [HttpGet("/")]
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      List<Vendor> allVendors = Vendor.GetAll();
-      return View(allVendors);
+
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
+
     }
-
-    [HttpGet("/vendors/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
-    }
-
-    [HttpPost("/vendors")]
-    public ActionResult Create(string name, string description)
-    {
-      Vendor myVendor = new Vendor(name, description);
-      return RedirectToAction("Index");
-    }
-
-
-
-  }
 }
---
+++ OrderTracker/Controllers/VendorsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using OrderTracker.Models;
+using System.Collections.Generic;
+
+namespace OrderTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string description)
+    {
+      Vendor myVendor = new Vendor(name, description);
+      return RedirectToAction("Index");
+    }
+  }
+}
