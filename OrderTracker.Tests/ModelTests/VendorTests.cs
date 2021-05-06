a67d330162b7cc7d5de747d09f5e4f912b33cdb7
Karlson Drendel <kdrendel99@gmail.com>
2021-05-06 13:29:15 -0700

    Create passing test to check for instantiation of Vendor

---
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using OrderTracker.Models;
+using System;
+
+namespace OrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("name","description");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
