b37446075496a8f42c9580dd1ba9e8f633b08855
Karlson Drendel <kdrendel99@gmail.com>
2021-05-06 16:19:45 -0700

    Refactored Vendor class to Order class, built new Vendor class and passing all tests.

---
+++ OrderTracker/Controllers/OrdersController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using OrderTracker.Models;
+using System.Collections.Generic;
+
+namespace OrderTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/orders")]
+    public ActionResult Index()
+    {
+      List<Order> allOrders = Order.GetAll();
+      return View(allOrders);
+    }
+
+    [HttpGet("/orders/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/orders")]
+    public ActionResult Create(string name, string title, string description, string price, string date)
+    {
+      Order myOrder = new Order(name, title, description, price, date);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/orders/delete")]
+    public ActionResult DeleteAll()
+    {
+      Order.ClearAll();
+      return View();
+    }
+
+    [HttpGet("/orders/{id}")]
+    public ActionResult Show(int id)
+    {
+      Order foundOrder = Order.Find(id);
+      return View(foundOrder);
+    }
+  }
+}
--- OrderTracker/Models/Vendor.cs
+++ OrderTracker/Models/Vendor.cs
@@ -4,32 +4,23 @@ namespace OrderTracker.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
     public string Description { get; set; }
     public int Id { get; }
-    private static List<Vendor> _instances = new List<Vendor> { };
+    public List<Order> Orders { get; set; }
 
-    public Vendor(string name, string description)
+    public Vendor(string vendorName)
     {
-      Name = name;
-      Description = description;
+      Name = vendorName;
       _instances.Add(this);
       Id = _instances.Count;
-    }
-    
-    public static List<Vendor> GetAll()
-    {
-      return _instances;
+      Orders = new List<Order>{};
     }
 
     public static void ClearAll()
     {
       _instances.Clear();
-    }
-
-    public static Vendor Find(int searchId)
-    {
-      return _instances[searchId-1];
     }
   }
 }
