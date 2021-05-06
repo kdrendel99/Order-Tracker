963036eaeb5926518d1490f970de961957ae2ddb
Karlson Drendel <kdrendel99@gmail.com>
2021-05-06 14:03:44 -0700

    Updated HomeController route decorators to use GET and POST

--- OrderTracker/Controllers/HomeController.cs
+++ OrderTracker/Controllers/HomeController.cs
@@ -5,12 +5,26 @@ namespace OrderTracker.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/")]
+    [HttpGet("/")]
     public ActionResult Index()
     {
       Vendor starterVendor = new Vendor("Vendor One","");
       return View(starterVendor);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string description)
+    {
+      Vendor myVendor = new Vendor(name, description);
+      return View("Index", myVendor);
+    }
+
 
 
   }
