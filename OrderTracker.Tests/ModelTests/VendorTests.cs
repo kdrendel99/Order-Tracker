b19c6547b13e80deaa8b4d97d506478a85e72aa6
Karlson Drendel <kdrendel99@gmail.com>
2021-05-06 13:37:24 -0700

    Create passing test to check for name accessibility in Vendor

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -20,5 +20,14 @@ namespace OrderTracker.Tests
       Vendor newVendor = new Vendor("name","description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string testName = "Coava Coffee";
+      Vendor newVendor = new Vendor("Coava Coffee","the description");
+      string result = newVendor.Name;
+      Assert.AreEqual(testName, result);     
+    }
   }
 }
