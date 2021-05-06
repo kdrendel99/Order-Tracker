bef1de4f10ba73666c9d267cfc552f107b575783
Karlson Drendel <kdrendel99@gmail.com>
2021-05-06 16:18:16 -0700

    Refactored Vendor class to be renamed Order to more accurately reflect functionality. Created new Vendor class and passing tests.

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -6,79 +6,79 @@ using System;
 namespace OrderTracker.Tests
 {
   [TestClass]
-  public class VendorTests : IDisposable
+  public class OrderTests : IDisposable
   {
 
     public void Dispose()
     {
-      Vendor.ClearAll();
+      Order.ClearAll();
     }
 
     [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Vendor newVendor = new Vendor("name","description");
-      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+      Order newOrder = new Order("name","description","test","test","test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
     [TestMethod]
     public void GetDescription_ReturnsDescription_String()
     {
       string testName = "Coava Coffee";
-      Vendor newVendor = new Vendor("Coava Coffee","the description");
-      string result = newVendor.Name;
+      Order newOrder = new Order("Coava Coffee","the description","test","test","test");
+      string result = newOrder.Name;
       Assert.AreEqual(testName, result);     
     }
 
     [TestMethod]
-    public void SetVendorName_SetNameString()
+    public void SetOrderName_SetNameString()
     {
       string name = "Bobs Coffee";
-      Vendor newVendor = new Vendor(name, "description");
+      Order newOrder = new Order(name, "description","test","test","test");
       string updatedName = "Marks Coffee";
-      newVendor.Name = updatedName;
-      string result = newVendor.Name;
+      newOrder.Name = updatedName;
+      string result = newOrder.Name;
       Assert.AreEqual(updatedName, result);
     }
 
     [TestMethod]
-    public void GetAll_ReturnsEmptyVendorList_VendorList()
+    public void GetAll_ReturnsEmptyOrderList_OrderList()
     {
-      List<Vendor> newList = new List<Vendor> { };
-      List<Vendor> result = Vendor.GetAll();
+      List<Order> newList = new List<Order> { };
+      List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
 
     [TestMethod]
-    public void GetAll_ReturnsVendors_VendorList()
+    public void GetAll_ReturnsOrders_OrderList()
     {
-      string name1 = "Vendor One";
-      string name2 = "Vendor Two";
-      Vendor newVendor1 = new Vendor(name1,"des");
-      Vendor newVendor2 = new Vendor(name2,"des");
-      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
-      List<Vendor> result = Vendor.GetAll();
+      string name1 = "Order One";
+      string name2 = "Order Two";
+      Order newOrder1 = new Order(name1,"des","test","test","test");
+      Order newOrder2 = new Order(name2,"des","test","test","test");
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+      List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
 
       [TestMethod]
-      public void GetId_VendorsInstantiateWithAnIdAndGetterReturns_Int()
+      public void GetId_OrdersInstantiateWithAnIdAndGetterReturns_Int()
       {
       string name = "Bob";
-      Vendor newVendor = new Vendor(name,"des");
-      int result = newVendor.Id;
+      Order newOrder = new Order(name,"des","test","test","test");
+      int result = newOrder.Id;
       Assert.AreEqual(1, result);
     }
 
     [TestMethod]
-    public void Find_ReturnsCorrectVendor_Vendor()
+    public void Find_ReturnsCorrectOrder_Order()
     {
-      string name1 = "Vendor 1";
-      string name2 = "Vendor 2";
-      Vendor newVendor1 = new Vendor(name1, "des1");
-      Vendor newVendor2 = new Vendor(name2, "des2");
-      Vendor result = Vendor.Find(2);
-      Assert.AreEqual(newVendor2, result);
+      string name1 = "Order 1";
+      string name2 = "Order 2";
+      Order newOrder1 = new Order(name1, "des1","test","test","test");
+      Order newOrder2 = new Order(name2, "des2","test","test","test");
+      Order result = Order.Find(2);
+      Assert.AreEqual(newOrder2, result);
     }
   }
 }
---
+++ OrderTracker.Tests/ModelTests/VendorTestsNew.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTracker.Tests
+{
+    [TestClass]
+  public class CategoryTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor= new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+  [TestMethod]
+  public void GetName_ReturnsName_String()
+  {
+    string name = "Test Vendor";
+    Vendor newVendor = new Vendor(name);
+    string result = newVendor.Name;
+    Assert.AreEqual(name, result);
+  }
+
+  [TestMethod]
+  public void GetId_ReturnsVendorId_Int()
+  {
+    string name = "Test Vendor";
+    Vendor newVendor = new Vendor(name);
+    int result = newVendor.Id;
+    Assert.AreEqual(1, result);
+  }
+
+  }
+}
