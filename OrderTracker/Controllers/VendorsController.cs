44aeedd30cbc882d9919040ab907aa480550e0db
Karlson Drendel <kdrendel99@gmail.com>
2021-05-06 14:23:46 -0700

    Build deleting vendors funcitonality.

--- OrderTracker/Controllers/VendorsController.cs
+++ OrderTracker/Controllers/VendorsController.cs
@@ -25,5 +25,12 @@ namespace OrderTracker.Controllers
       Vendor myVendor = new Vendor(name, description);
       return RedirectToAction("Index");
     }
+
+    [HttpPost("/vendors/delete")]
+    public ActionResult DeleteAll()
+    {
+      Vendor.ClearAll();
+      return View();
+    }
   }
 }
